df6f43d2c26bb17fb201a962d2869de5f18c90ed
mobi12 <mc1a1@hotmail.com>
2018-06-30 20:41:36 +0800

    目前在研究使用Canvas画棋盘

--- Tic-Tac/MainWindow.xaml.cs
+++ Tic-Tac/MainWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace Tic_Tac
         public MainWindow()
         {
             InitializeComponent();
+            DrawingChessBoard(ChessBoard);
+        }
+
+        private static void DrawingChessBoard(Canvas canvas) //只能画一条直线
+        {
+            LineGeometry lineGeometry = new LineGeometry();
+            Point startPoint = new Point(100, 0);
+            Point endPoint = new Point(100, 300);
+            Path path = new Path();
+
+            lineGeometry.StartPoint = startPoint;
+            lineGeometry.EndPoint = endPoint;
+            path.Stroke = Brushes.Red;
+            path.StrokeThickness = 1;
+            path.Data = lineGeometry;
+
+            canvas.Children.Add(path);
         }
     }
 }
