35b005fd945452214028a0fd4f5d942e545bde14
mobi12 <mc1a1@hotmail.com>
2018-07-02 21:44:10 +0800

    很快就可以把这个小游戏编写出来了，写出来还需要重构

--- Tic-Tac/MainWindow.xaml.cs
+++ Tic-Tac/MainWindow.xaml.cs
@@ -23,14 +23,13 @@ namespace Tic_Tac
         public MainWindow()
         {
             InitializeComponent();
-            DrawingChessBoard(ChessBoard);
         }
 
-        private static void DrawingChessBoard(Canvas canvas) //只能画一条直线
+        private void DrawingElement(Canvas canvas, Point start, Point end)
         {
             LineGeometry lineGeometry = new LineGeometry();
-            Point startPoint = new Point(100, 0);
-            Point endPoint = new Point(100, 300);
+            Point startPoint = start;
+            Point endPoint = end;
             Path path = new Path();
 
             lineGeometry.StartPoint = startPoint;
@@ -40,6 +39,32 @@ namespace Tic_Tac
             path.Data = lineGeometry;
 
             canvas.Children.Add(path);
+        }
+
+        private void DrawingChessX(Point location)
+        {
+            Point startPointOfLeft = new Point(location.X + 20, location.Y + 20);
+            Point endPointOfLeft = new Point(location.X - 20, location.Y - 20);
+            Point startPointOfRight = new Point(location.X - 20, location.Y + 20);
+            Point endPointOfRight = new Point(location.X + 20, location.Y - 20);
+
+            DrawingElement(ChessBoard, startPointOfLeft, endPointOfLeft);
+            DrawingElement(ChessBoard, startPointOfRight, endPointOfRight);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingElement(ChessBoard, new Point(0, 80), new Point(300, 80));
+            DrawingElement(ChessBoard, new Point(0, 160), new Point(300, 160));
+            DrawingElement(ChessBoard, new Point(100, 0), new Point(100, 300));
+            DrawingElement(ChessBoard, new Point(200, 0), new Point(200, 300));
+            StartButton.IsEnabled = false;
+            StartButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingChessX(new Point(47, 38));
         }
     }
 }
